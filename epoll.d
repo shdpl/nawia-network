16530b4193d382e125af86246614a390b4ef22dc
Mariusz `shd` Gliwiński <alienballance@gmail.com>
2015-08-03 15:41:59 +0200

    tmp

--- epoll.d
+++ epoll.d
@@ -4,11 +4,13 @@ private import std.bitmanip;
 private import core.sys.linux.epoll;
 private import std.exception : errnoEnforce;
 private import std.socket;
+private import std.conv;
 
 alias int EpollSocket;
 alias int SetCreateFlags;
 alias epoll_event Event;
 alias int StandardSocket;
+alias to!int toInt;
 
 
 
@@ -58,6 +60,19 @@ EpollSocket modify( EpollSocket set, StandardSocket socket, ref Event event )
 	return ret==0 ? set : ret;
 }
 
+EpollSocket wait( EpollSocket set, ref Event event, int timeout )
+{
+	auto ret = epoll_wait( set, &event, 1, timeout );
+	return ret==0 ? set : ret;
+}
+
+EpollSocket wait( EpollSocket set, Event[] events, int timeout, out Event[] result )
+{
+	auto ret = epoll_wait( set, events.ptr, toInt( events.length ), timeout );
+	result = events[0..ret];
+	return ret==0 ? set : ret;
+}
+
 Event readable( Event event, bool value )
 {
 	auto flag = EPOLLIN;
@@ -69,6 +84,12 @@ Event readable( Event event, bool value )
 	{
 		event.events &= ~flag;
 	}
+	return event;
+}
+
+Event readable( Event event, out bool value )
+{
+	value = event.events & EPOLLIN ? true : false;
 	return event;
 }
 
@@ -86,6 +107,12 @@ Event writable( Event event, bool value )
 	return event;
 }
 
+Event writable( Event event, out bool value )
+{
+	value = event.events & EPOLLOUT ? true : false;
+	return event;
+}
+
 Event readablePriority( Event event, bool value )
 {
 	auto flag = EPOLLPRI;
@@ -97,6 +124,12 @@ Event readablePriority( Event event, bool value )
 	{
 		event.events &= ~flag;
 	}
+	return event;
+}
+
+Event readablePriority( Event event, out bool value )
+{
+	value = event.events & EPOLLPRI ? true : false;
 	return event;
 }
 
@@ -179,21 +212,43 @@ void main()
 	auto readEvent = Event
 		.init
 		.readable( true )
+		.edgeTriggerred( true )
 	;
 	auto writeEvent = Event
 		.init
 		.writable( true )
+		.readable( true )
 	;
-	writeln( SetCreateFlags.init
+	Event[] readOut;
+	immutable ubyte[] data = [1,2,3,4];
+	sockets[0].send( data );
+	readEvent.data.fd = cast(int) sockets[1].handle;
+	writeEvent.data.fd = cast(int) sockets[0].handle;
+	writeln( [sockets[1].handle, sockets[0].handle] );
+	writeln( [readEvent.data.fd,writeEvent.data.fd] );
+	auto socketSet = SetCreateFlags.init
 		.create
 		.errnoEnforce( "Could not create epoll set" )
-		.add( cast(int) sockets[0].handle, readEvent )
+		.add( cast(int) sockets[1].handle, readEvent )
 		.errnoEnforce( "Could not add reading socket to epoll set" )
-		.add( cast(int) sockets[1].handle, writeEvent )
+		.add( cast(int) sockets[0].handle, writeEvent )
 		.errnoEnforce( "Could not add writing socket to epoll set" )
-		.remove( cast(int) sockets[0].handle, readEvent )
+		.wait( [readEvent,writeEvent], 0, readOut );
+		.errnoEnforce( "Could wait for sockets" )
+	;
+	foreach( Event event; readOut )
+	{
+		bool r, w, p;
+		event.readable( r ).writable( w ).readablePriority( p );
+		writeln( "flags", [EPOLLIN, EPOLLOUT, EPOLLPRI, EPOLLERR, EPOLLRDHUP] );
+		writeln( [r, w, p], [event.events, event.events&EPOLLIN], [event.events, event.events&EPOLLOUT], event.data.fd );
+	}
+	auto buf = new ubyte[data.length];
+	sockets[1].receive(buf);
+	socketSet
+		.remove( cast(int) sockets[1].handle, readEvent )
 		.errnoEnforce( "Could not remove reading socket to epoll set" )
-		.remove( cast(int) sockets[1].handle, writeEvent )
+		.remove( cast(int) sockets[0].handle, writeEvent )
 		.errnoEnforce( "Could not remove writing socket to epoll set" )
-	);
+	;
 }
